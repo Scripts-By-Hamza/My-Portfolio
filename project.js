a75fecf6b7fb2d162bf850214418c0ba26f7bee2
Muhammad Hamza <rajpoothamza2412@gmail.com>
2025-08-16 10:16:53 +0500

    Updated Portfolio

---
+++ project.js
@@ -0,0 +1,38 @@
+const projects = [
+    {
+        name: "Learning Management System",
+        image: "./assets/project-1.0.png",
+        github: "https://github.com/Scripts-By-Hamza/LMS-Project",
+        live: "",
+    },
+    {
+        name: "Bubble Game JavaScript",
+        image: "./assets/project-2.0.png",
+        github: "https://github.com/Scripts-By-Hamza/Bubble-Project",
+        live: "https://scripts-by-hamza.github.io/Bubble-Project/",
+    },
+    {
+        name: "Apex Bazar E-Commerce",
+        image: "./assets/project-3.0.png",
+        github: "https://github.com/Scripts-By-Hamza/Apex-bazzar-E-Commerce",
+        live: "",
+    },
+    {
+        name: "Blog Post Project",
+        image: "./assets/project-4.0.png",
+        github: "https://github.com/Scripts-By-Hamza/Blog-Post-Website",
+        live: "https://scripts-by-hamza.github.io/Blog-Post-Website/",
+    },
+    {
+        name: "Tool Grid Project",
+        image: "./assets/project-5.0.png",
+        github: "https://github.com/Scripts-By-Hamza/ToolGrid",
+        live: "https://scripts-by-hamza.github.io/ToolGrid/",
+    },
+    {
+        name: "Food Restaurants",
+        image: "./assets/project-6.0.png",
+        github: "https://github.com/Scripts-By-Hamza/Food-Restaurants-Website",
+        live: "https://scripts-by-hamza.github.io/Food-Restaurants-Website/",
+    },
+]
--- script.js
+++ script.js
@@ -4,3 +4,39 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const projectContent = document.getElementById('projects-js');
+
+    if (projectContent && typeof projects !== 'undefined') {
+        projectContent.innerHTML = projects.map(project => `
+
+        <div class="details-container color-container">
+            <div class="article-container">
+              <img
+                src="${project.image}"
+                alt="Project 1"
+                class="project-img"
+              />
+            </div>
+            <h2 class="experience-sub-title project-title">
+              ${project.name}
+            </h2>
+            <div class="btn-container">
+              <button
+                class="btn btn-color-2 project-btn"
+                onclick="location.href='${project.github}'"
+              >
+                Github
+              </button>
+               <button
+                class="btn btn-color-2 project-btn"
+                onclick="location.href='${project.live}'"
+              >
+                Live Demo
+              </button>
+            </div>
+        </div>
+        `).join('');
+    }
+});
